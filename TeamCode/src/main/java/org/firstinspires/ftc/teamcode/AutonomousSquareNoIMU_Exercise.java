78acfbbb21885c2b2f694492689d23ac68250f74
markfontecchio <58674223+markfontecchio@users.noreply.github.com>
2020-09-24 18:05:47 -0400

    Mark Fontecchio update 9/24/20

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousSquareNoIMU_Exercise.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousSquareNoIMU_Exercise.java
@@ -24,8 +24,17 @@ public class AutonomousSquareNoIMU_Exercise extends LinearOpMode {
         if (opModeIsActive())
         {
             // autonomous code goes here
+            driveFL.setPower(0.5);
+            driveFR.setPower(0.5);
+            driveBL.setPower(0.5);
+            driveBR.setPower(0.5);
 
+            sleep(2000);
 
+            driveFL.setPower(0);
+            driveFR.setPower(0);
+            driveBL.setPower(0);
+            driveBR.setPower(0);
 
 //            timedDrive("forward", 0.5, 1000); // drives forward at 0.5 motor power for 1000 milliseconds
 //            timedDrive("backward", 0.5, 2000); //drives backward at 0.5 motor power for 2000 milliseconds
